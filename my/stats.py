84d5f7ecc4923520415a6d0e9249ff10de59ba0a
Laszlo Treszkai <laszlo.treszkai@gmail.com>
2022-11-22 08:55:56 +0100

    refactor(stats): use pattern matching
    Sorry, Python 3.9 users.

--- my/stats.py
+++ my/stats.py
@@ -8,12 +8,18 @@ def compare_ratios(a: float | tuple[int, int], b: float | tuple[int, int]):
     tuple of ints, representing (number of successes, total number of events),
     assuming a uniform prior.
     """
-    if isinstance(a, float) and isinstance(b, float):
-        return 0.5 if a == b else float(a < b)
-    elif isinstance(a, float) and isinstance(b, tuple):
-        return st.beta.sf(a, b[0] + 1, b[1] - b[0] + 1)
-    elif isinstance(b, float) and isinstance(a, tuple):
-        return 1 - compare_ratios(b, a)
-    else:
-        # TODO
-        raise NotImplementedError
+    match a, b:
+        case (float(), float()):
+            return 0.5 if a == b else float(a < b)
+        case (float(), (int(), int())):
+            b_success, b_total = b
+            return st.beta.sf(a, b_success + 1, b_total - b_success + 1)
+        case ((int(), int()), float()):
+            return 1 - compare_ratios(b, a)
+        case ((int(), int()), (int(), int())):
+            # TODO
+            raise NotImplementedError('comparing two tuples is not implemented yet')
+        case _:
+            raise TypeError(
+                'Every argument of compare_ratios must be either a float or a pair of integers.'
+            )
